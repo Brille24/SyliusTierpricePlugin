cd43e93df4ad5b6ee81ad0e377d3cb515ea3e84e
mamazu <maximilian.pesch@brille24.de>
2018-01-10 11:41:46 +0100

    Replaced the default tests with Brille24 tests

---
+++ tests/Behat/Context/TierpriceContext.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mamazu
+ * Date: 09/01/18
+ * Time: 11:32
+ */
+
+
+namespace Brille24\Behat\Context;
+
+
+use Behat\Behat\Context\Context;
+use Brille24\TierPriceBundle\Entity\ProductVariant;
+use Brille24\TierPriceBundle\Entity\TierPrice;
+use Sylius\Component\Core\Model\ProductInterface;
+
+final class TierpriceContext implements Context
+{
+
+    /**
+     * @Given /^"([^"]*)" has a tier price at (\d+) with ("[^"]+")$/
+     */
+    public function producthasATierPrice(ProductInterface $product, int $quantity, int $price): void
+    {
+        /** @var ProductVariant $productVariant */
+        $productVariant = array_values($product->getVariants())[0];
+
+        $tierPrice = $this->createTierprice($quantity, $price);
+        $productVariant->addTierPrice($tierPrice);
+    }
+
+    private function createTierPrice(int $quantity, $price): TierPrice
+    {
+        $tierPrice = new TierPrice($quantity);
+        $tierPrice->setPrice((int) (floatval($price)*100));
+
+        return $tierPrice;
+    }
+}

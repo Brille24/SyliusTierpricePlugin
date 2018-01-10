2ac420b5dca49063c317febca2875ac680473453
mamazu <maximilian.pesch@brille24.de>
2018-01-10 11:40:17 +0100

    Updated the plugin configuration

--- src/Brille24/TierPriceBundle/Brille24TierPriceBundle.php
+++ src/Brille24/TierPriceBundle/Brille24TierPriceBundle.php
@@ -2,8 +2,13 @@
 
 namespace Brille24\TierPriceBundle;
 
+use Brille24\TierPriceBundle\DependencyInjection\Brille24TierPriceExtension;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class Brille24TierPriceBundle extends Bundle
 {
+    public function getContainerExtension()
+    {
+        return new Brille24TierPriceExtension();
+    }
 }

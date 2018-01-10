8a702282cdc1db4ba2c64999db20e8068ea814d9
mamazu <maximilian.pesch@brille24.de>
2018-01-10 12:06:09 +0100

    Removed loading of config files in the extension

--- src/Brille24/TierPriceBundle/DependencyInjection/Brille24TierPriceExtension.php
+++ src/Brille24/TierPriceBundle/DependencyInjection/Brille24TierPriceExtension.php
@@ -13,9 +13,5 @@ class Brille24TierPriceExtension extends Extension
     {
         $configuration = new Configuration();
         $config        = $this->processConfiguration($configuration, $configs);
-
-        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
-        $loader->load('services.yml');
-        $loader->load('resources.yml');
     }
 }

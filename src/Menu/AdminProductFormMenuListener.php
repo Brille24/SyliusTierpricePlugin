e3c45ca99ef305c1280e94d4804b44c0c59b4f78
mamazu <maxo.pech@gmail.com>
2018-10-25 20:33:31 +0200

    Fixed bug with configureable products

--- src/Menu/AdminProductFormMenuListener.php
+++ src/Menu/AdminProductFormMenuListener.php
@@ -11,7 +11,7 @@ declare(strict_types=1);
 
 namespace Brille24\SyliusTierPricePlugin\Menu;
 
-use Sylius\Bundle\UiBundle\Menu\Event\MenuBuilderEvent;
+use Sylius\Bundle\AdminBundle\Event\ProductMenuBuilderEvent;
 use Symfony\Component\Translation\Translator;
 
 final class AdminProductFormMenuListener
@@ -26,12 +26,17 @@ final class AdminProductFormMenuListener
         $this->translator = $translator;
     }
 
-    public function addItems(MenuBuilderEvent $event): void
+    public function addItems(ProductMenuBuilderEvent $event): void
     {
         $menu = $event->getMenu();
 
+        if($event->getProduct()->isConfigurable()){
+            return;
+        }
+
         $TIERPRICE_TAB = '@Brille24SyliusTierPricePlugin/Resources/views/Admin/ProductVariant/Tab/_tierprice.html.twig';
-        $menu->addChild('tierprice')
+        $menu
+            ->addChild('tierprice')
             ->setAttribute('template', $TIERPRICE_TAB)
             ->setLabel($this->translator->trans('brille24_tier_price.ui.tier_prices'))
             ->setLabelAttribute('icon', 'dollar');

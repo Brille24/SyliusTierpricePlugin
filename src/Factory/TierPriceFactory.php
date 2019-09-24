6087a63788f1aacb2933e1a3a2b1a1d2bedcb8ba
Mark Beazley <mark@bluebanana.com>
2019-09-24 09:21:55 +0100

    Apply new ECS standard

--- src/Factory/TierPriceFactory.php
+++ src/Factory/TierPriceFactory.php
@@ -1,12 +1,14 @@
 <?php
+
 /**
  * This file is part of the Brille24 tierprice plugin.
  *
  * (c) Brille24 GmbH
  *
- * For the full copyright and licence information, please view the LICENCE
+ * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
+
 declare(strict_types=1);
 
 namespace Brille24\SyliusTierPricePlugin\Factory;
--- src/Form/TierPriceType.php
+++ src/Form/TierPriceType.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * This file is part of the Brille24 tierprice plugin.
  *
@@ -7,6 +8,7 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
+
 declare(strict_types=1);
 
 namespace Brille24\SyliusTierPricePlugin\Form;
@@ -20,8 +22,6 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Validator\Constraints\NotBlank;
-use Symfony\Component\Validator\Constraints\Range;
 
 class TierPriceType extends AbstractType
 {

107d486ffff16ed4170680b9d8715284640d6600
mamazu <maxo.pech@gmail.com>
2020-05-21 22:29:21 +0200

    Updating the test application

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -31,13 +31,6 @@ final class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $treeBuilder->root('brille24_tierprice');
-
-        // Here you should define the parameters that are allowed to
-        // configure your bundle. See the documentation linked above for
-        // more information on that topic.
-
-        return $treeBuilder;
+        return new TreeBuilder('brille24_tierprice');
     }
 }

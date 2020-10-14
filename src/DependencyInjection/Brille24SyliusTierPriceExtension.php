ac87da5ee0a0c8236601a2a146ea0dacebd884b6
Jakob Tolkemit <jakob.tolkemit@brille24.de>
2020-10-14 13:10:07 +0200

    Updated to Sylius 1.8

--- src/DependencyInjection/Brille24SyliusTierPriceExtension.php
+++ src/DependencyInjection/Brille24SyliusTierPriceExtension.php
@@ -16,14 +16,34 @@ namespace Brille24\SyliusTierPricePlugin\DependencyInjection;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 
-final class Brille24SyliusTierPriceExtension extends Extension
+final class Brille24SyliusTierPriceExtension extends Extension implements PrependExtensionInterface
 {
     /** {@inheritdoc} */
     public function load(array $config, ContainerBuilder $container): void
     {
         $this->processConfiguration($this->getConfiguration([], $container), $config);
         new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+    }
+
+    public function prepend(ContainerBuilder $container): void
+    {
+        if (!$container->hasExtension('doctrine_migrations') || !$container->hasExtension('sylius_labs_doctrine_migrations_extra')) {
+            return;
+        }
+
+        $container->prependExtensionConfig('doctrine_migrations', [
+            'migrations_paths' => [
+                'Brille24\SyliusTierPricePlugin\Migrations' => '@Brille24SyliusSpecialPricePlugin/Migrations',
+            ],
+        ]);
+
+        $container->prependExtensionConfig('sylius_labs_doctrine_migrations_extra', [
+            'migrations' => [
+                'Brille24\SyliusTierPricePlugin\Migrations' => ['Sylius\Bundle\CoreBundle\Migrations'],
+            ],
+        ]);
     }
 }

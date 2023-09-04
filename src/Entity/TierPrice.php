703391ed14c8827725a478a085dad558a800ae0d
mamazu <14860264+mamazu@users.noreply.github.com>
2023-09-04 12:00:53 +0200

    Attributes

--- src/Entity/TierPrice.php
+++ src/Entity/TierPrice.php
@@ -14,22 +14,40 @@ declare(strict_types=1);
 
 namespace Brille24\SyliusTierPricePlugin\Entity;
 
+use Brille24\SyliusTierPricePlugin\Repository\TierPriceRepository;
+use Doctrine\ORM\Mapping\Column;
+use Doctrine\ORM\Mapping\Entity;
+use Doctrine\ORM\Mapping\Id;
+use Doctrine\ORM\Mapping\ManyToOne;
+use Doctrine\ORM\Mapping\Table;
+use Doctrine\ORM\Mapping\UniqueConstraint;
 use Sylius\Component\Core\Model\ChannelInterface;
 use Sylius\Component\Customer\Model\CustomerGroupInterface;
 
+#[Entity(repositoryClass:TierPriceRepository::class)]
+#[Table(name: 'brille24_tierprice')]
+#[UniqueConstraint(name: 'no_duplicate_prices', columns: ['qty','channel_id','product_variant_id','customer_group_id'])]
 class TierPrice implements TierPriceInterface
 {
     /** @var int */
+    #[Id, Column(type: 'integer')]
     private $id;
 
+    #[ManyToOne(targetEntity: ChannelInterface::class)]
     private ?ChannelInterface $channel = null;
 
+    #[ManyToOne(targetEntity: ProductVariantInterface::class, inversedBy: 'tierPrices')]
     private ProductVariantInterface $productVariant;
 
+    #[ManyToOne(targetEntity: CustomerGroupInterface::class, inversedBy: 'customerGroup')]
     private ?CustomerGroupInterface $customerGroup = null;
 
-    public function __construct(private int $qty = 0, private int $price = 0)
-    {
+    public function __construct(
+        #[Column(type: 'integer')]
+        private int $qty = 0,
+        #[Column(type: 'integer')]
+        private int $price = 0
+    ) {
     }
 
     public function getId(): ?int

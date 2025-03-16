49b48245e14459c5739c8885ea5aaf1ed08a2b3e
mamazu <14860264+mamazu@users.noreply.github.com>
2025-03-16 12:04:22 +0100

    Removing brille24 from the licence

--- ecs.php
+++ ecs.php
@@ -3,7 +3,7 @@
 /**
  * This file is part of the Brille24 tierprice plugin.
  *
- * (c) Brille24 GmbH
+ * (c) Mamazu
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -34,7 +34,7 @@ return static function (ECSConfig $ecsConfig): void {
         'header' => <<<DOC
 This file is part of the Brille24 tierprice plugin.
 
-(c) Brille24 GmbH
+(c) Mamazu
 
 For the full copyright and license information, please view the LICENSE
 file that was distributed with this source code.
--- src/Form/Components/ProductVariantFormComponent.php
+++ src/Form/Components/ProductVariantFormComponent.php
@@ -3,7 +3,7 @@
 /**
  * This file is part of the Brille24 tierprice plugin.
  *
- * (c) Brille24 GmbH
+ * (c) Mamazu
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
